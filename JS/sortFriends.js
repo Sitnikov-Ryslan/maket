71768e6c3bbe1c2b058b752c26eed88ac3ca197e
Sitnikov-Ryslan <63663502+Sitnikov-Ryslan@users.noreply.github.com>
2020-05-13 15:20:44 +0400

    Add files via upload

--- JS/sortFriends.js
+++ JS/sortFriends.js
@@ -1,71 +1,77 @@
 const initialFriends = document.querySelectorAll('.friend');
+const initialFriendsArray = Array.prototype.slice.call(initialFriends);
 
 let friendsList = document.querySelectorAll('.friend');
 let parent = document.querySelector('.friends');
 let btns = document.querySelectorAll('.btn');
 let btnOriginal = document.querySelector('.btn--original');
 let btnOnline = document.querySelector('.btn--online');
+let btnAbc = document.querySelector('.btn--abc');
+
+/*Сортировка к начальному(оригинальному) состоянию*/
 
 btnOriginal.addEventListener("click", function(event) {
   event.preventDefault();
+  for (let btn of btns) {
+    btn.classList.remove('checked');
+  }
+  btnOriginal.classList.add('checked');
   
+  let presentFriends = document.querySelectorAll('.friend');
+  let presentFriendsArray = [];
+  let friendsParent = presentFriends[0].parentNode;
+  for (let i = 0; i < presentFriends.length; i++) {    
+    presentFriendsArray.push(friendsParent.removeChild(presentFriends[i]));
+  }
+  presentFriendsArray.sort((nodeA,nodeB) => 
+      initialFriendsArray.indexOf(nodeA) - initialFriendsArray.indexOf(nodeB)
+  )
+  .forEach(function(node) {
+      friendsParent.appendChild(node)
+    });
 });
 
+/*Сортировка друзей, которые онлайн*/
 
 btnOnline.addEventListener("click", function(event) {
   event.preventDefault();
+  for (let btn of btns) {
+    btn.classList.remove('checked');
+  };
+  btnOnline.classList.add('checked');
+
 	for (let i = 0; i <= friendsList.length - 1; i++) {
   		let friend = friendsList[i];
   		let online = friend.querySelector('.online');
   		if (online !== null) {
   			parent.prepend(friend);
-        btns.classList.remove('checked');
-        btnOnline.classList.add('checked');
-  		};
-  	};
-  	// let newFriendsList = parent.querySelectorAll('.friend');
-	let sort = function(aRestore) { // !!! - параметр
-	var nodeList = parent.querySelectorAll('.friend');
-	var itemsArray = [];
-	var listParent = nodeList[0].parentNode;
+      };
+  };
+});
 
-	for (var i = 0; i < nodeList.length; i++) {
-    // !!! - здесь
-	    if (!nodeList[i].dataset.originalorder)
-			nodeList[i].dataset.originalorder = i + 1;
-		    itemsArray.push(listParent.removeChild(nodeList[i]));
-	}
+/*Сортировка по алфавиту*/
 
-	itemsArray.sort(function(nodeA, nodeB) {
-    // !!! - и здесь
-    if (aRestore) {
-		return nodeA.dataset.originalorder - nodeB.dataset.originalorder;
-    }
-
-		let nameA = nodeA.querySelector('.friendName').textContent.toLowerCase();
-		let nameB = nodeB.querySelector('.friendName').textContent.toLowerCase();
-	    if (nameA < nameB) return -1;
-	    if (nameA > nameB) return 1;
-	    return 0;
-		}).forEach(function(node) {
-		    listParent.appendChild(node)
-		});
-};
-  	// let friendsListArray = Array.prototype.slice.call(newFriendsList);
-  	// let onlineFriendsList = [];
-  	// 	for (let item of newFriendsList) {
-  	// 		if (item.querySelector('.online') !== null) {
-  	// 			onlineFriendsList.push(item);
-  	// 		}
-  	// 	}
-  	// 	// Сортировка массива
-  	// 	onlineFriendsList.sort(function(a, b) {
-  	// 		let nameA = a.querySelector('.friendName').textContent.toLowerCase();
-  	// 		let nameB = b.querySelector('.friendName').textContent.toLowerCase();
-  	// 		console.log('A = ' + nameA);
-  	// 		console.log('B = ' + nameB);
-  	// 		if (nameA < nameB) return -1
-  	// 			if (nameA > nameB) return 1
-  	// 				return 0
-  	// 	});
+btnAbc.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (let btn of btns) {
+    btn.classList.remove('checked');
+  };
+  btnAbc.classList.add('checked');
+	
+  let nodeList = document.querySelectorAll('.friend');
+  let itemsArray = [];
+  let nodeListParent = nodeList[0].parentNode;
+  for (let i = 0; i < nodeList.length; i++) {    
+    itemsArray.push(nodeListParent.removeChild(nodeList[i]));
+  }
+  itemsArray.sort(function(nodeA, nodeB) {
+      let nameA = nodeA.querySelector('.friendName').textContent.toLowerCase();
+      let nameB = nodeB.querySelector('.friendName').textContent.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    })
+    .forEach(function(node) {
+      nodeListParent.appendChild(node)
+    });
 });
